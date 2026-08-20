9eb307a43c5a7dd662fff145bdc8e288c7f31b79
denor1999 <ozhgixinv@list.ru>
2026-08-20 15:17:51 +0300

    edit build.gradle: added testLogging

--- src/test/java/appleTests/TestBase.java
+++ src/test/java/appleTests/TestBase.java
@@ -3,10 +3,8 @@ package appleTests;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
-import static com.codeborne.selenide.Selenide.open;
 
 public class TestBase {
 
